574f2d02559aff9e236d6b8f6161b8dc4bad92e3
Victor Harbo Johnston <vhol@kb.dk>
2022-09-21 10:40:42 +0200

    Add getDominantColor code

--- src/main/java/dk/kb/image/Facade.java
+++ src/main/java/dk/kb/image/Facade.java
@@ -2,10 +2,19 @@ package dk.kb.image;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
+
+import com.ctc.wstx.cfg.OutputConfigFlags;
+
 import java.awt.image.BufferedImage;
 
 public class Facade {
@@ -48,12 +57,12 @@ public class Facade {
         // Hashset is a collection that only has one of each value
         Set<Integer> colors = new HashSet<Integer>();
         // get image's width and height
-        int w = img.getWidth();
-        int h = img.getHeight();
+        int width = img.getWidth();
+        int height = img.getHeight();
 
         // Nested for loop that adds unique colors to the hashset colors
-        for(int y = 0; y < h; y++) {
-            for(int x = 0; x < w; x++) {
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
                 int pixel = img.getRGB(x, y);     
                 colors.add(pixel);
             }
@@ -62,5 +71,42 @@ public class Facade {
         String result = "There are " + Integer.toString(colors.size()) + " colors in this picture.";
 
         return result;
+    }
+
+
+    public static void getDominantColor(BufferedImage img){
+        // Create array long enough to hold all RGB colors
+        int[] allPixelsArray = new int[16777216];
+
+
+        // ArrayList is an array that can be added to.
+        // get image's width and height
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        // Loop over all pixels in image and get RGB color
+        for(int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int pixel = img.getRGB(x, y);
+                // add +1 to array at RGB index
+                allPixelsArray[pixel & 0x00FFFFFF] += 1;
+            }
+        }
+
+        int maxRGB = 0;
+        int maxCount = 0;
+
+        // Checks all posible RGB colors in array
+        for (int rgb = 0; rgb < 16777215; rgb++){
+            if (allPixelsArray[rgb] != 0) {
+                if (allPixelsArray[rgb] > maxCount){
+                    maxCount = allPixelsArray[rgb];
+                    maxRGB = rgb;
+                }
+            }
+        }
+
+        System.out.println(Integer.toString(maxRGB, 16));
+        System.out.println(maxCount);
     }
 }
