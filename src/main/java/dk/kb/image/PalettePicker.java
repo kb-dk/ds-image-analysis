f065ea581ea1752929d479dba3039bc75bd7feb2
Victor Harbo Johnston <vhol@kb.dk>
2022-09-30 14:17:32 +0200

    Add TODO

--- src/main/java/dk/kb/image/PalettePicker.java
+++ src/main/java/dk/kb/image/PalettePicker.java
@@ -45,6 +45,8 @@ public class PalettePicker {
         return smkOkLabBuckets;
     }
 
+    // TODO: Run DeltaE distance between all SMK colors
+
     /**
      * Create a string[] of 256 hex colors used by SMK (National Gallery of Denmark).
      * It is not clear how this palette has been constructed in the first place.
