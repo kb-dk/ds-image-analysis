f8aab91c7ab8b884fa6f9449a462412b13eb507d
Victor Harbo Johnston <vhol@kb.dk>
2022-09-30 15:38:23 +0200

    Add analyse of smk colors and test

---
+++ src/test/java/dk/kb/image/SmkColorTest.java
@@ -0,0 +1,10 @@
+package dk.kb.image;
+
+import org.junit.jupiter.api.Test;
+
+public class SmkColorTest {
+    @Test
+    public void testSMKColors(){
+        AnalyseSmkDeltaE.AnalyseColorDistance();
+    }
+}
