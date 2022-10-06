27f76bdd814e79d70526ec528a793b894e0bded8
Victor Harbo Johnston <vhol@kb.dk>
2022-10-06 10:16:53 +0200

    Update TODOs for missing tests

--- src/test/java/dk/kb/image/PalettePickerTest.java
+++ src/test/java/dk/kb/image/PalettePickerTest.java
@@ -1,5 +1,9 @@
 package dk.kb.image;
 
 public class PalettePickerTest {
-    
+    //TODO: Add defineSimpleBuckets test
+    //TODO: Add arbitraryOKlabBuckets test
+    //TODO: Add smkOkLabBuckets test
+    //TODO: add smkRgbBuckets test
+    //TODO: add smkHexPalette test
 }
--- src/test/java/dk/kb/image/RgbColorTest.java
+++ src/test/java/dk/kb/image/RgbColorTest.java
@@ -1,5 +1,8 @@
 package dk.kb.image;
 
 public class RgbColorTest {
-    
+    //TODO: Add countBucketsForImg test
+    //TODO: Add updateBucketCounter test
+    //TODO: add getEuclidianColorDistance test
+    //TODO: add printResult test
 }
