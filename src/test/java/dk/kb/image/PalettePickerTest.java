0a76f8b96a07897bee8022baeda2ed4898b6ade0
Victor Harbo Johnston <vhol@kb.dk>
2022-10-07 13:05:51 +0200

    remove unused test class

--- src/test/java/dk/kb/image/PalettePickerTest.java
+++
@@ -1,9 +0,0 @@
-package dk.kb.image;
-
-public class PalettePickerTest {
-    //TODO: Add defineSimpleBuckets test
-    //TODO: Add arbitraryOKlabBuckets test
-    //TODO: Add smkOkLabBuckets test
-    //TODO: add smkRgbBuckets test
-    //TODO: add smkHexPalette test
-}
