2ca847efef51ddd39e1b0c295fd9ece81b9e272d
Victor Harbo Johnston <vhol@kb.dk>
2022-11-09 15:37:49 +0100

    refactor rgb unit tests

--- src/test/java/dk/kb/image/MostUsedRgbColorsTest.java
+++ src/test/java/dk/kb/image/MostUsedRgbColorsTest.java
@@ -1,25 +1,41 @@
 package dk.kb.image;
 
-import dk.kb.util.Resolver;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.imageio.ImageIO;
+import dk.kb.util.Resolver;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import javax.imageio.ImageIO;
 
-public class MostUsedRgbColorsTest {
+public class RgbColorTest {
     private Logger log = LoggerFactory.getLogger(this.toString());
 
     @Test
-    public void getMostUsedRgbColor() throws IOException {
+    public void testCountBucketsForImg() throws IOException{
         BufferedImage img;
+        List<Integer> buckets = PalettePicker.defineSimpleBuckets();
         img = ImageIO.read(Resolver.resolveStream("flower.jpg"));
-        MostUsedColors myColors = new MostUsedRgbColors();
-        assertFalse(myColors.getMostUsedColors(img, 5).isEmpty());
-        log.info("The most used color has been calculated");
+        int[] result = MostUsedRgbColors.countBucketsForImg(img, buckets);
+        assertTrue(result[3] == 22089);
+        log.info("Bucket counter divides pixels to correct buckets.");
+    }
+
+    @Test
+    public void testGetEuclidianColorDistance(){
+        int red = Color.RED.getRGB();
+        int pink = Color.PINK.getRGB();
+        int blue = Color.BLUE.getRGB();
+
+        int redResult = MostUsedRgbColors.getEuclidianColorDistance(red, pink);
+        int blueResult = MostUsedRgbColors.getEuclidianColorDistance(blue, pink);
+        assertTrue(redResult<blueResult);
+        log.info("Pink color is closer to red than to blue as expected.");
     }
 }
